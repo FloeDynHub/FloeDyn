92967d788d076b87c2b0d789cd81b5c1e64d0d12
Quentin Jouet <quentin.jouet@gmail.com>
2017-02-01 10:48:37 +0100

    Add vortexes (storm mode) to explicit physical datas

--- src/floe/dynamics/explicit_physical_data.hpp
+++ src/floe/dynamics/explicit_physical_data.hpp
@@ -7,10 +7,12 @@
 #ifndef OPE_EXPLICIT_PHYSICAL_DATA_HPP
 #define OPE_EXPLICIT_PHYSICAL_DATA_HPP
 
+#include "floe/geometry/arithmetic/arithmetic.hpp"
 #include "floe/geometry/arithmetic/point_operators.hpp"
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 // #include <iostream> // DEBUG
 
 
@@ -36,7 +38,9 @@ public:
 
     //! Constructor
     ExplicitPhysicalData(value_type const& time_ref) : m_time_ref{time_ref},
-        m_window_width{1}, m_window_height{1}, m_water_mode{0}, m_air_mode{4} {}
+        m_window_width{1}, m_window_height{1},
+        m_vortex_radius{0}, m_vortex_origin{0,0}, m_vortex_speed{0,0}, m_vortex_max_norm{0},
+        m_water_mode{0}, m_air_mode{4} {}
 
     point_type water_speed(point_type pt = {0,0}) {
         return get_speed(pt, m_water_mode);
@@ -63,11 +67,46 @@ public:
         m_air_mode = air_mode;
     }
 
+    void set_storm_mode(){
+        init_random_vortex();
+        m_air_mode = 5;
+        m_water_mode = 0;
+    }
+
+    void init_random_vortex(){
+        value_type avg_Rc{300000}, delta_Rc{110000};
+        value_type avg_V{8}, delta_V{4};
+        value_type min_Umax{15}, max_Umax{30};
+        auto dist_Rc = std::uniform_real_distribution<double>{
+            avg_Rc - delta_Rc, avg_Rc + delta_Rc};
+        auto dist_V = std::uniform_real_distribution<double>{
+            avg_V - delta_V, avg_V + delta_V};
+        auto dist_Umax = std::uniform_real_distribution<double>{
+            min_Umax, max_Umax};
+        auto dist_angle = std::uniform_real_distribution<double>{
+            0, 2 * M_PI};
+        auto gen = std::default_random_engine{};
+        gen.seed(std::time(0));
+        m_vortex_radius = dist_Rc(gen);
+        m_vortex_max_norm = dist_Umax(gen);
+        value_type theta = dist_angle(gen);
+        // 500km from {0,0} todo : less rigid choice !
+        m_vortex_origin = 500000 * point_type{cos(theta), sin(theta)};
+        m_vortex_speed = - dist_V(gen) * point_type{cos(theta), sin(theta)};
+    }
+
 private:
 
     value_type const& m_time_ref; //!< reference to time variable
+    // window dimension (for generator)
     value_type m_window_width;
     value_type m_window_height;
+    // vortex attributes
+    value_type m_vortex_radius;
+    point_type m_vortex_origin;
+    point_type m_vortex_speed;
+    value_type m_vortex_max_norm;
+    // modes
     int m_water_mode;
     int m_air_mode;
 
@@ -81,6 +120,8 @@ private:
                 return convergent_outside_window_circular_inside_field(pt);
             case 4:
                 return x_convergent_then_constant(pt);
+            case 5:
+                return vortex(pt);
             default:
                 return {0,0};
         }
@@ -123,9 +164,21 @@ private:
             return constant;
     }
 
+    point_type vortex_center(){
+        return m_vortex_origin + m_time_ref * m_vortex_speed;
+    }
+
     //! x axis convergent wind, then constant
-    point_type vortex(point_type pt = {0,0}, value_type coeff = 10, point_type constant = {10,0}) {
-        return {0,0};
+    point_type vortex(point_type pt) {
+        point_type vortex_center_to_pt = pt - vortex_center();
+        value_type distance_to_center = norm2(vortex_center_to_pt);
+        if (distance_to_center < m_vortex_radius){
+            return (m_vortex_max_norm / m_vortex_radius) *
+                floe::geometry::direct_orthogonal(vortex_center_to_pt);
+        } else {
+            return m_vortex_max_norm * (m_vortex_radius / distance_to_center) *
+                floe::geometry::direct_orthogonal(vortex_center_to_pt) / distance_to_center;
+        }
     }
 };
 
